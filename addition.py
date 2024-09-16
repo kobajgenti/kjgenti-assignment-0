592d9d81d51342d7174edcd9aae4518e42420c8f
kobajgenti <95399025+kobajgenti@users.noreply.github.com>
2024-09-16 10:47:56 -0400

    Create addition.py
    added assignment file

---
+++ addition.py
@@ -0,0 +1,12 @@
+def add_numbers(a, b):
+    return a + b
+
+# Input two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum
+sum_result = add_numbers(num1, num2)
+
+# Print the result
+print("The sum of", num1, "and", num2, "is:", sum_result)
